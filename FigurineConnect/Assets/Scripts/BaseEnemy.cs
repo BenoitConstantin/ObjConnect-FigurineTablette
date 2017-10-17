66d40a20efe0ac3849c14435ac1581ff6e35f94d
seamusoneill <seamusoneill@live.ie>
2017-10-17 16:28:19 +0200

    ADD: -Navmesh Tools and test scene update

---
+++ FigurineConnect/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//Nav Mesh Agent
+public class BaseEnemy : MonoBehaviour {
+
+    protected uint health = 50;
+    protected uint attack = 10;
+    protected Vector2 target;
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
