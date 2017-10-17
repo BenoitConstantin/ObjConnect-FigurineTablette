2e9d2e8893fef8f0fc02d06885bfb3aa1e9c8f69
DESKTOP-I6S9LOU\Joan <yusuke.joan@gmail.com>
2017-10-17 19:10:49 +0200

    - Base Level Updated. - FInished Shooter unit, it needs a good polished.

--- FigurineConnect/Assets/Scripts/Player/Bullets/SimpleBullet.cs
+++ FigurineConnect/Assets/Scripts/Player/Bullets/SimpleBullet.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleBullet : MonoBehaviour {
+    public Vector3 direction;
+    public float speed;
+    [Range(0,1)]
+    public float scaleTime = 1;
+    private Vector3 position;
+    private Rigidbody2D rigid;
+    private Vector3 limitInf;
+    private Vector3 limitSup;
+    private Vector3 forward;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        GameObject limits = GameObject.FindGameObjectWithTag("Limits");
+        if (limits != null) {
+            limitInf = limits.transform.Find("Min").localPosition;
+            limitSup = limits.transform.Find("Max").localPosition;
+        } else {
+            Debug.LogError("Please create and tag a Limits GameObject with Min and Max GameObjects to define the map limits");
+        }
+	}
 
-	// Use this for initialization
-	void Start () {
-		
+    private void Update() {
+        Time.timeScale = scaleTime;
+        Vector3 pos = transform.localPosition;
+        if(pos.x<limitInf.x || pos.y<limitInf.y || pos.x> limitSup.x || pos.y> limitSup.y) {
+            ///Destroy(gameObject);
+        }
+    }
+
+    public void SetDirection(Vector3 direction, float angle) {
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        //Debug.Log(direction);
+        this.direction = Vector3.Normalize(direction);
+        //this.forward = forward;
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        Move();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Move() {
+        rigid.MovePosition(transform.position + direction * (Time.deltaTime * speed*100));
+    }
 }
--- FigurineConnect/Assets/Scripts/Player/Unities/ShooterUnit.cs
+++ FigurineConnect/Assets/Scripts/Player/Unities/ShooterUnit.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class ShooterUnit : BaseUnit {
 
+    public float cadence; //Shoot frequence
+    public GameObject bullet;
+    public Transform head;
+    private float currentTime;
+    private Transform bulletLayer;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        bulletLayer = GameObject.FindGameObjectWithTag("BulletLayer").transform;
+        SetCamera();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0)) {
+            SetNewPosition(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        currentTime += Time.deltaTime;
+        if (currentTime > cadence) {
+            Shoot();
+            currentTime = 0;
+        }
+        Rotate();
 	}
+
+    void Shoot() {
+        GameObject newBullet = Instantiate(bullet);
+        newBullet.transform.parent = (bulletLayer != null)? bulletLayer: transform.parent;
+        newBullet.transform.localPosition = transform.localPosition;
+        newBullet.GetComponent<SimpleBullet>().SetDirection(head.position - transform.position, rotation);
+    }
 }
