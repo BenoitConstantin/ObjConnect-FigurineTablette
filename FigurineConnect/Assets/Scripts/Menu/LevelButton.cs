98a8f4383f52615fab1b534db14734decbd32d8e
CONSTANTIN <benoitc@equilibregames.com>
2017-10-17 13:45:06 +0200

    -ADD : 4 level scenes

--- FigurineConnect/Assets/Scripts/Menu/LevelButton.cs
+++ FigurineConnect/Assets/Scripts/Menu/LevelButton.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour {
 
     [SerializeField]
     int levelNumber = 1;
 
+    [SerializeField]
+    Text text; 
 
     public void SelectLevel()
     {
         GameManager.Instance.LoadLevel(levelNumber);
     }
 
+    public void SetLevelNumber(int number)
+    {
+        this.gameObject.name = "Level_" + number;
+        this.levelNumber = number;
+        text.text = "Level " + number;
+    }
 }
