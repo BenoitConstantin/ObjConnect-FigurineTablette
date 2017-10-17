6f7f52e886b332edd4b40aa399030dc6528eb4b5
DESKTOP-I6S9LOU\Joan <yusuke.joan@gmail.com>
2017-10-17 16:34:52 +0200

    -ADD: Prefabs and base classes

---
+++ FigurineConnect/Assets/Scripts/Player/Unities/BaseUnit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUnit : MonoBehaviour {
+
+    public bool inGame { get; private set; }
+    public float actionTime;
+
+    // Use this for initialization
+    void Start () {
+        inGame = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
