ec1f40ff0383ad29865b1ddad2e27b39758e8155
CONSTANTIN <benoitc@equilibregames.com>
2017-10-19 11:11:52 +0200

    -ADD : Menu calibration

---
+++ FigurineConnect/Assets/Scripts/Menu/CalibrationButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalibrationButton : MonoBehaviour {
+
+    [SerializeField]
+    Text surfaceObjectNameText;
+
+    CalibrationMenu calibrationMenu;
+    SurfaceObject surfaceObject;
+
+	public void Init(CalibrationMenu calibrationMenu, SurfaceObject surfaceObject)
+    {
+        this.calibrationMenu = calibrationMenu;
+        this.surfaceObject = surfaceObject;
+
+        surfaceObjectNameText.text = surfaceObject.id;
+    }
+
+    public void Calibrate()
+    {
+        calibrationMenu.Calibrate(this.surfaceObject);
+    }
+
+}
